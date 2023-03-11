19aed8ae7917048283001e6bad7fb98a529d3e58
Adebowale <adebowaletemitayo40@gmail.com>
2023-03-11 20:56:43 +0100

    Implementing the projects Section

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <>
       <Navbar />
       <Home />
-      {/* <Main />
-      <Footer /> */}
+      <Main />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./main.css";
-
+import img from "../../img/img1.jpg";
 const Data = [
   {
     id: 1,
@@ -8,15 +8,44 @@ const Data = [
     title: "Multi-Post Stories Gain+Glory",
     tools: ["html", "css", "JavaScript", "reactJS"],
   },
+  //   {
+  //     id: 2,
+  //     imgSrc: img,
+  //     title: "Multi-Post Stories Gain+Glory",
+  //     tools: ["html", "css", "JavaScript", "reactJS"],
+  //   },
+  //   {
+  //     id: 3,
+  //     imgSrc: img,
+  //     title: "Multi-Post Stories Gain+Glory",
+  //     tools: ["html", "css", "JavaScript", "reactJS"],
+  //   },
 ];
 
 const Main = () => {
   return (
     <section className="main container section">
       <div className="secTitle">
-        <h3 className="title">My Recent Works</h3>
+        <h2 className="title">My Recent Works</h2>
       </div>
-      <div className="secContent grid"></div>
+      <div className="secContent grid">
+        {Data.map(({ id, imgSrc, title, tools }) => {
+          return (
+            <div key={id} className="singleProject">
+              <div className="imageDiv">
+                <img src={imgSrc} alt={title} />
+              </div>
+
+              <div className="projContent">
+                <h3 className="projTitle">{title}</h3>
+                {tools.map((tool, id) => {
+                  return <span key={id}>{tool}</span>;
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
