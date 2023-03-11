ebb199d4e5b14c5bba3a1da550eec13bbf758e20
Adebowale <adebowaletemitayo40@gmail.com>
2023-03-11 19:27:51 +0100

    Implemented the Home component

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import home from "../../img/home.png";
-
+import { Link } from "react-router-dom";
+import { BsTwitter } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
 import "./home.css";
 
 const Home = () => {
@@ -13,13 +17,28 @@ const Home = () => {
         <div className="textDiv flex">
           <h1 className="textTitle">
             <span className="top">Hey there. I’m Damilola</span> <br />
-            <span className="top">I’m a software developer</span>
+            <span className="low">I’m a software developer</span>
           </h1>
           <p className="paragraph">
             I can help you build a product , feature or website Look through
             some of my work and experience! If you like what you see and have a
             project you need coded, don’t hestiate to contact me.
           </p>
+
+          <div className="homeIcons flex">
+            <a className="icons" to="/#">
+              <BsTwitter className="icon" />
+            </a>
+            <a className="icons" to="/#">
+              <FaLinkedinIn className="icon" />
+            </a>
+            <a className="icons" to="/#">
+              <FaFacebookF className="icon" />
+            </a>
+            <a className="icons" to="/#">
+              <BsGithub className="icon" />
+            </a>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 import "./main.css";
-const Main = () => {};
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img,
+    title: "Multi-Post Stories Gain+Glory",
+    tools: ["html", "css", "JavaScript", "reactJS"],
+  },
+];
+
+const Main = () => {
+  return (
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 className="title">My Recent Works</h3>
+      </div>
+      <div className="secContent grid"></div>
+    </section>
+  );
+};
 
 export default Main;
